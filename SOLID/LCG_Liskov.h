557db8e18caa25f181465187fcd4ed07e132aebe
DillFennel <julea.basik@gmail.com>
2026-09-17 19:26:26 +0300

    Liscov

---
+++ SOLID/LCG_Liskov.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <list>
+
+using namespace std;
+
+
+random_device rd;
+mt19937 gen(rd());
+
+struct LCG {
+    virtual int generate() = 0;
+    int state;
+    LCG(){}
+    void addSeed(int _seed){
+	    state = _seed;
+	    srand(_seed);
+    }
+    LCG(int _seed){
+        state = _seed;
+        srand(_seed);
+    }
+    int generate_100(){
+        int r = rand();
+        if(r < 0){ r *= -1; }
+        r = r%101;
+        return r;
+    }
+};
+
+struct LCG_t_p : public LCG {
+    using LCG::LCG;
+
+    int generate() override {
+        int r = generate_100();
+        if(r < 25){
+            return 1;
+        }
+        if(r < 48){
+            return 2;
+        }
+        if(r < 69){
+            return 3;
+        }
+        if(r < 81){
+            return 4;
+        }
+        if(r < 90){
+            return 5;
+        }
+        return 6;
+    }
+};
+
+struct LCG_t_obr : public LCG { 
+    using LCG::LCG;
+
+    int generate() override {
+        int r = generate_100();
+        if(r < 6){
+            return 1;
+        }
+        if(r < 21){
+            return 2;
+        }
+        if(r < 48){
+            return 3;
+        }
+        if(r < 77){
+            return 4;
+        }
+        if(r < 83){
+            return 5;
+        }
+        return 6;
+    }
+};
--- SOLID/test_distribution.cpp
+++ SOLID/test_distribution.cpp
@@ -2,28 +2,29 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include "LCG.h"
+#include "LCG_Liskov.h"
 
 using namespace std;
 
 void make_data(int seed = 10){ // n - количество колок
     int n = 9000; // Все кол-во запусков
     int k = 3; // Кол-во отрезков для проверки
-    LCG lcg(seed);
+    LCG_t_p lcg_t_p(seed);
+    LCG_t_obr lcg_t_obr;
     int t_p, t_obr;
     vector<vector<int>> t_p_count(k, vector<int> (6, 0));
     vector<vector<int>> t_obr_count(k, vector<int> (6, 0));
     for(int k1=1; k1<k+1; k1++){
         for(int i=n/k*(k1-1)+1; i<n/k*k1; i++){
-            t_p = lcg.generate_t_p();
-            t_obr =lcg.generate_t_obr();
+            t_p = lcg_t_p.generate();
+            t_obr = lcg_t_obr.generate();
             t_p_count[k1-1][t_p-1]++;
             t_obr_count[k1-1][t_obr-1]++;
         }
     }
     int pre;
-    std::ofstream fout;          // поток для записи
-    fout.open("test_distribution_data.txt");      // открываем файл для записи
+    std::ofstream fout; // поток для записи
+    fout.open("test_distribution_data.txt"); // открываем файл для записи
     fout << n << " " << k << endl;
     fout << "t_p" <<endl;
     for(int k1 = 0; k1 < k; k1++){
